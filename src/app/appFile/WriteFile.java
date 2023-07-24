fff21af461d687f14f3d98365e7fee4d49aa2bfc
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-24 16:44:22 +0300

    Add BufferedWriter in method writeFile

--- src/app/appFile/WriteFile.java
+++ src/app/appFile/WriteFile.java
@@ -3,6 +3,8 @@ package app.appFile;
 import app.data.DataN;
 import app.util.Constants;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,9 +25,12 @@ public class WriteFile {
     public static String writeFile(String nameFile, ArrayList<DataN> list){
         Path file = Path.of(Constants.BASE_IN_PATH + nameFile + ".doc");
         try {
-            String content = String.valueOf(list);
-            byte[] text = content.getBytes();
-            Files.write(file, text);
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter(file.toFile(), true));
+           for(DataN data : list){
+               writer.write(data.toString());
+               writer.newLine();
+           }
             return "Данні у файл записані ";
         } catch (IOException e) {
             return "Помилка запису данних " +  e.getMessage();
