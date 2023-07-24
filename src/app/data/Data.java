901931c6892f66101d89271873a29623d45a314c
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-24 14:27:05 +0300

    fix cod in Data class

--- src/app/data/Data.java
+++ src/app/data/Data.java
@@ -6,15 +6,16 @@ import java.util.Scanner;
 
 public class Data {
 
-    public static List<String > getData(){
+    public static List<DataN> getData(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ведіть назву продукту ");
         String name = scanner.nextLine();
         System.out.println("Ведіть ціну продукту");
         String price = scanner.nextLine();
-        List<String> list = new ArrayList<>();
-        list.add(name);
-        list.add(price);
-         return list;
+        List<DataN> list = new ArrayList<>();
+        list.add(new DataN(name, price));
+        return list;
     }
+
+
 }
